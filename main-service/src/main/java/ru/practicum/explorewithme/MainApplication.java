3dab70b0e2a88147dba57ab0c0477ea556be351a
Bogdan <bogdan@MacBook-Air-Bogdan.local>
2026-03-01 21:10:37 +0100

    main service

--- main-service/src/main/java/ru/practicum/explorewithme/MainApplication.java
+++ main-service/src/main/java/ru/practicum/explorewithme/MainApplication.java
@@ -2,6 +2,6 @@ package ru.practicum.explorewithme;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Main service is not implemented on stat_svc stage.");
+        System.out.println("заглушка для main-service");
     }
 }
