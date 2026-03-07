a0146698dd8745f9d4f82237f00bcbe711562a20
Pavel Motyrev <0520249@gmail.com>
2026-03-07 13:03:43 +0300

    add category controller

---
+++ main-service/src/main/java/ru/practicum/explorewithme/dto/category/NewCategory.java
@@ -0,0 +1,4 @@
+package ru.practicum.explorewithme.dto.category;
+
+public class NewCategory {
+}
---
+++ main-service/src/main/java/ru/practicum/explorewithme/service/category/CategoryService.java
@@ -0,0 +1,4 @@
+package ru.practicum.explorewithme.service.category;
+
+public class CategoryService {
+}
